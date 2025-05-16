c2e977e1526773ef53213830976e958a28dbaa3b
johnohhh1 <john.olenski@gmail.com>
2025-05-16 15:56:39 -0400

    fix: always merge googleDrive state in handleFolderSelect to preserve accessToken and other fields

--- src/components/GoogleDriveAuth.jsx
+++ src/components/GoogleDriveAuth.jsx
@@ -25,6 +25,7 @@ export default function GoogleDriveAuth() {
       addNewImages(images, 'googleDrive');
       updateSettings({
         googleDrive: {
+          ...settings.googleDrive,
           isConnected: true,
           selectedFolder: folder
         }
